e566603df4b04bc1b1dd792ae528d253bf54228d
Kim KeeHyun <kimkee7@gmail.com>
2019-11-01 13:46:01 +0900

    work  링크2

--- js/ui.js
+++ js/ui.js
@@ -48,6 +48,100 @@ $(document).ready(function () {
 	});
 
 
+
+	var sdRandom = Math.ceil((  Math.random() * 4)  );
+	$('#slides').slidesjs({
+		width: 700,
+		height: 506,
+		autostart: false,
+		start: 1,//sdRandom
+		play: {
+			active: true,
+			// [boolean] Generate the play and stop buttons.
+			// You cannot use your own buttons. Sorry.
+			effect: "slide",
+			// [string] Can be either "slide" or "fade".
+			interval: 2000,
+			// [number] Time spent on each slide in milliseconds.
+			auto: false,
+			// [boolean] Start playing the slideshow on load.
+			swap: true,
+			// [boolean] show/hide stop and play buttons
+			pauseOnHover: true,
+			// [boolean] pause a playing slideshow on hover
+			restartDelay: 2500
+			// [number] restart delay on inactive slideshow
+		},
+		navigation: {
+			active: true,
+			effect: "slide"
+		},
+		pagination: {
+			active: true,
+			effect: "slide"
+		},
+		effect: {
+			slide: {
+			speed: 200
+			},
+			fade: {
+			speed: 300,
+			crossfade: true
+			}
+		}
+	});
+	
+	var linkStat = false;
+
+	var linkAmtFunc = function(){
+		$(".contain .container .pList li .ptBox .ss a   ,   .mainTop .mainSd .slides .pBox a").each(function(index) {
+			var linkAmt = $(this).attr("href");
+			$(this).attr("href","javascript:;").css("cursor","default").removeAttr("target");
+			$(this).attr("data-url",linkAmt);
+		});
+		console.log("링크가 비활성화 됐습니다.")
+	}
+	// linkAmtFunc();
+	
+	var linkAmtSet = function(){
+		$(".contain .container .pList li .ptBox .ss a   ,   .mainTop .mainSd .slides .pBox a").each(function(index) {
+			var linkAmt = $(this).attr("data-url");
+			$(this).attr("href",linkAmt).css("cursor","pointer").attr("target","_blank");
+			
+		});
+		console.log("링크가 활성화 됐습니다.")
+	}
+	//linkAmtSet()
+
+	$(document).keydown(function(event){ // M 키 이벤트
+		if(event.keyCode == 77 ){
+			if(!linkStat){
+				linkAmtSet();
+				linkStat = true;
+			}else{
+				linkAmtFunc();
+				linkStat = false;
+			}
+		}
+	});	
+	
+
+	$("#scrollbar1").mCustomScrollbar({
+		mouseWheel:{
+			scrollAmount:400,
+			preventDefault:true
+		}	
+	});
+
+
+	$("img.lazy").lazyload({
+		event : "fadeIn"
+	});
+	$("img.lazy").show().lazyload();
+	
+
+	console.log("김기현 - kimkee@naver.com , 010-3236-1677");
+
 });
 
 
