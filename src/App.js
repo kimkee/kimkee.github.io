5bc4112e8f92bee3353be339876d9bfcd3d356a4
KimKeeHyun <kimkee7@gmail.com>
2023-02-08 12:37:47 +0900

    App

--- src/App.js
+++ src/App.js
@@ -7,18 +7,100 @@ import GitHubButton from 'react-github-btn';
 
 
 function App() {
-  // const [menuItems, setMenuItems] = useState([]);
-  let data;
-  useEffect(() => {
-    console.log('React Hooks are working!!');
+  const [data, setData] = useState();
+  const fetchJson = () => {
+    fetch("./js/data.json")
+    .then(response => {
+      return response.json();
+    }).then(data => {
+      setData(data);
+      console.log(data.update);
+    }).catch((e) => {
+      console.log(e.message);
+    });
+  }
 
-  } );
+  useEffect(() => {
+    fetchJson();
+
+    
+  },[]);
+
+	const renderTech = (pjt) => {  // 프로젝트 스킬 아이콘
+		const  skcls = {
+			"HTML": "icotech html",
+			"CSS": "icotech css",
+			"SASS": "icotech css",
+			"JS": "icotech js",
+			"Mobile": "icotech mobile",
+			"PC": "icotech pc",
+			"Respond": "icotech mobile",
+			"Design": "icotech design",
+			"Flash": "icotech flash",
+			"ActionScript": "icotech flash" ,
+		};
+		const result =  pjt.tech.map( (pjt, idx) => {
+			// console.log(skcls[pjt]);
+			// skcls[pjt] == undefined ? skcls[pjt] = "icotech" : null;
+			return( <em key={idx} className={skcls[pjt]}>{pjt}</em> );
+		});
+		return result;
+	};
+
+	const renderPsld = () => { // 상단 슬라이드
+		const result = data.puix.map( (pjt, idx) => {
+      if(idx < 16 ) {console.log();}
+      return (
+        <li key={idx} className="swiper-slide pbox">
+          <a href="#none" data-url={ pjt.urls }><img className="img" src={ pjt.imgs } alt="SS" /></a>
+          <div className="info">
+            <div className="pack">{ renderTech(pjt) }</div>
+            <div className="date"><i className="d">{ pjt.date }</i><i className="p">{ pjt.plce }</i></div>
+            <div className="name">{ pjt.tits }</div> <div className="screen"></div>
+          </div>
+        </li>
+      )
+			
+		});
+		return result ;
+	};
+
+
+	const renderPtxt = () => {  //  프로젝트 리스트 스크롤 영역
+		const result = data.puix.map( (pjt, idx) => {
+			return (
+				<li key={idx}>
+					<div className="date"><i className="d">{ pjt.date }</i><i className="p">{ pjt.plce }</i></div>
+					<div className="name">{ pjt.tits }</div>
+				</li>
+			)
+		});
+		return result ;
+	};
+	const renderPlist = (pwork) => { // 프로젝트 리스트 uix,deg,fla
+		// console.log(wdata[pwork]);
+		const result = data[pwork].map( (cate, idx) => {
+			return (
+				<li key={idx}>
+					<div className="pbox">
+						{<div className="pack">{ data && renderTech(cate) }</div>}
+						<div className="ss"><a href="#none" data-url={ cate.urls }><img className="img" data-original={ cate.imgs } src={ cate.imgs } alt="SS" loading="lazy" /></a></div>
+						<div className="name">{ cate.tits }</div>
+						<div className="info">
+							<span className="date">{ cate.date }</span> - <span className="place">{ cate.plce }</span>
+						</div>
+					</div>
+				</li>
+			)
+		});
+		return result ;
+	};
 
 
 
   return (
     <>
-      <Header update={data}></Header>
+      <Header update={data?.update} assign={data?.assign}></Header>
       <article className="visuals">
 				<div className="inr">
 					
@@ -26,7 +108,7 @@ function App() {
 			
 					<section className="mnslide">
 						<div className="inr swiper-container" id="slides">
-							<ul className="swiper-wrapper slides" id="data_psld">{/* {renderPsld()} */}</ul>
+							<ul className="swiper-wrapper slides" id="data_psld">{data && renderPsld()}</ul>
 							<div className="navi">
 								<button type="button" className="nav prev">이전</button><button type="button" className="nav next">다음</button>
 							</div>
@@ -36,14 +118,14 @@ function App() {
 			
 					<section className="profile">
 						<div className="salestat">
-							{/* {
-								wdata.opened == true
+							{
+								data?.opened === true
 								? <img src="./img/cm/forSale.png" className="img sale" alt="QQQ FORSALE" />
 								: <img src="./img/cm/soldOut.png" className="img sold" alt="QQQ SOLDOUT" />
-							} */}
+							}
 						</div>
 						<div className="box">
-							<div className="photo"><img className="img" src="./img/cm/photo.jpg" alt="사진" /></div>
+              <div className="photo"><img className="img" src="./img/cm/photo.jpg" alt="사진" /></div>
 							<div className="info">
 								<div className="name">김기현</div>
 								<div className="phone"><a href="tel:010-3236-1677">010-3236-1677</a></div>
@@ -56,7 +138,7 @@ function App() {
 						<div className="career">
 							<div className="inr">
 								<div className="ut-scrolls">
-									<ul className="list">{/* renderPtxt() */}</ul>
+									<ul className="list">{ data && renderPtxt()}</ul>
 								</div>
 							</div>
 						</div>
@@ -65,9 +147,9 @@ function App() {
 				</div>
 			</article>
 
-      <article className="notice"  /* style={ wdata.opened == true ? {"display": "block"} : {"display": "none"} } */>
+      <article className="notice"  style={ data?.opened === true ? {"display": "block"} : {"display": "none"} }>
 				<div className="box">
-					<span className="msg">{/* wdata.assign */} 부터 프로젝트 <strong>투입</strong>가능합니다!</span>
+					<span className="msg">{data?.assign} 부터 프로젝트 <strong>투입</strong>가능합니다!</span>
 					<button type="button" className="close"><span>닫기</span></button>
 				</div>
 			</article>
@@ -78,21 +160,21 @@ function App() {
 					<section className="mplist">
 						<div className="hdts"><h3 className="title">UI/UX Development</h3></div>
 						<div className="plists">
-							<ul className="list ui">{/* renderPlist("puix") */}</ul>
+							<ul className="list ui">{data && renderPlist("puix")}</ul>
 						</div>
 					</section>
 			
 					<section className="mplist">
 						<div className="hdts"><h3 className="title">Web Design &amp; UI/UX Development</h3></div>
 						<div className="plists">
-							<ul className="list dg">{/* renderPlist("pdeg") */}</ul>
+							<ul className="list dg">{data && renderPlist("pdeg")}</ul>
 						</div>
 					</section>
 					
 					<section className="mplist">
 						<div className="hdts"><h3 className="title">Flash ActionScript</h3></div>
 						<div className="plists">
-							<ul className="list fa">{/* renderPlist("pfla") */}</ul>
+							<ul className="list fa">{data && renderPlist("pfla")}</ul>
 						</div>
 					</section>
 			
@@ -112,30 +194,16 @@ function App() {
 }
 
 
-function Header (){
+function Header ({update,assign}){
   
+  console.log(update, assign);
   
-  
-  const [data, setData] = useState();
-  const fetchJson = () => {
-    fetch("https://kimkee.github.io/js/data.json")
-    .then(response => {
-      return response.json();
-    }).then(data => {
-      setData(data);
-      console.log(data.update);
-    }).catch((e) => {
-      console.log(e.message);
-    });
-  }
-  useEffect(() => {
-    fetchJson()
-  },[])
+
   
   return(
 		<header className="header">
 			<div className="inr">
-				<div className="date">{data?.update} </div>
+				<div className="date">{update} </div>
 				<span className="github">
           <GitHubButton href="https://github.com/kimkee" aria-label="Follow @kimkee on GitHub">Follow @kimkee</GitHubButton>
 				</span>
@@ -150,10 +218,12 @@ function Skills(){
 			<div className="title">Works List Page of Kim KeeHyun</div>
 			<div className="skills">
 				<span className="box">
-					<img className="browsers" src="./img/cm/icoBrowsers.png" alt="Cross Browsers" />
+					{/* <img className="browsers" src="./img/cm/icoBrowsers.png" alt="Cross Browsers" /> */}
 					<img className="html5" src="./img/cm/icoHTML5.png" alt="HTML5" />
 					<img className="css3" src="./img/cm/icoCSS3.png" alt="CSS3" />
 					<img className="js" src="./img/cm/icoJS.png" alt="js" />
+					<img className="js" src="./img/cm/icoVue.png" alt="Vue" />
+					<img className="js" src="./img/cm/icoReact.png" alt="React" />
 					<img className="jquery" src="./img/cm/icoJquery.png" alt="jQuery" />
 					<img className="sass" src="./img/cm/icoSass.png" alt="SASS" />
 					<img className="less" src="./img/cm/icoLess.png" alt="LESS" />
