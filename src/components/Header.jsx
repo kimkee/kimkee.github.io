c4ff3a838c06270c068acc38926dbeaccfc66f91
KimKeeHyun <kimkee7@gmail.com>
2023-04-01 11:10:30 +0900

    Update - {ver}

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,14 @@ import React from "react";
 import GitHubButton from 'react-github-btn';
 
 export default  function Header ({update}){
+  const ver =  (process.env.REACT_APP_VER).slice(0,10)
   return(
     <header className="header">
       <div className="inr">
-        <div className="date">{update} {process.env.REACT_APP_VER}</div>
+        <div className="date">
+          {/* {update} */}
+          Update - {ver}
+        </div>
         <span className="github">
           <GitHubButton href="https://github.com/kimkee" aria-label="Follow @kimkee on GitHub">Follow @kimkee</GitHubButton>
         </span>
