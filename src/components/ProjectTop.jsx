19b2927df35ca30ef813ca76dcb0018020217884
KimKeeHyun <kimkee7@gmail.com>
2023-07-10 13:19:53 +0900

    .

--- src/components/ProjectTop.jsx
+++ src/components/ProjectTop.jsx
@@ -38,25 +38,21 @@ export default  function ProjectTop({data, cate }){
           // scrollbar={{ draggable: true }}
           initialSlide={ Math.floor( Math.random() * 10 ) }
           onSwiper={(swiper) => console.log()}
-          onSlideChange={() => { /* console.log('slide change') */}  }   >
-            {
-              data[cate].filter( (pjt, i) => pjt.hide !== true ).filter( (pjt, i) => i < 10 ).map( (pjt, idx) => {
-                  return (
-                    <SwiperSlide tag="li" key={idx}  className="swiper-slide">
-                      <Link className="pbox" to={""+cate+'/'+idx}  data-url={ pjt.urls }>
-                        <span className='lk' to={""+cate+'/'+idx}  data-url={ pjt.urls }>
-                          <img className="img" src={ import.meta.env.VITE_REACT_APP_PUBLIC_URL+pjt.imgs } alt={pjt.tits} loading="lazy" />
-                        </span>
-                        <div className="info">
-                          <div className="pack">{ <Tech pjt={pjt}/> }</div>
-                          <div className="date"><i className="d">{ pjt.date }</i><i className="p">{ pjt.plce }</i></div>
-                          <div className="name">{ pjt.tits }</div> <div className="screen"></div>
-                        </div>
-                      </Link>
-                    </SwiperSlide>
-                  )
-              })
-            }
+          onSlideChange={() => { /* console.log('slide change') */}  }   > {
+            data[cate].filter( (pjt, i) => pjt.hide !== true ).filter( (pjt, i) => i < 10 ).map( (pjt, idx) => { return (
+            <SwiperSlide tag="li" key={idx}  className="swiper-slide">
+              <Link className="pbox" to={""+cate+'/'+idx}  data-url={ pjt.urls }>
+                <span className='lk' to={""+cate+'/'+idx}  data-url={ pjt.urls }>
+                  <img className="img" src={ import.meta.env.VITE_REACT_APP_PUBLIC_URL+pjt.imgs } alt={pjt.tits} loading="lazy" />
+                </span>
+                <div className="info">
+                  <div className="pack">{ <Tech pjt={pjt}/> }</div>
+                  <div className="date"><i className="d">{ pjt.date }</i><i className="p">{ pjt.plce }</i></div>
+                  <div className="name">{ pjt.tits }</div> <div className="screen"></div>
+                </div>
+              </Link>
+            </SwiperSlide>
+            )})}
         </Swiper>
 
       </div>
